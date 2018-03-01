97a02b6cd33248de19ad35477dfe48239eaa4782
Artem Sokolov <artemsokolov@hotmail.com>
2018-03-01 21:38:06 +0300

    Initial commit

---
+++ lang/ru/.description.php
@@ -0,0 +1,5 @@
+<?
+$MESS ['IBLOCK_ELEMENT_ADD_FORM_NAME'] = "Расширенная форма добавления / редактирования";
+$MESS ['IBLOCK_ELEMENT_ADD_FORM_DESCRIPTION'] = "Расширенная форма добавления / редактирования элемента инфоблока";
+$MESS ['T_IBLOCK_DESC_ELEMENT_ADD'] = "Добавление элементов";
+?>
